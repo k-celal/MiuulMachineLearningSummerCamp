04fccbf170a27ae624a4737dc1c43504d77be10a
k-celal <celal51kara@gmail.com>
2023-08-10 11:45:46 +0300

    new study

--- Data_Analysis/data_analysis.py
+++ Data_Analysis/data_analysis.py
@@ -132,7 +132,7 @@ s.index
 s.dtype
 s.size
 s.ndim
-s.values
+s.values#numpy array döner
 type(s.values)
 s.head(3)
 s.tail(3)
@@ -156,8 +156,8 @@ df.shape
 df.info()
 df.columns
 df.index
-df.describe().T
-df.isnull().values.any()
+df.describe().T #transpoz daha okunabilir yapar
+df.isnull().values.any() #eksik değer var mı
 df.isnull().sum()
 df["sex"].head()
 df["sex"].value_counts()
@@ -167,11 +167,254 @@ import pandas as pd
 import seaborn as sns
 df = sns.load_dataset("titanic")
 df.index
-df[0:10]
+df[0:10]#0 dahil 10 hariç
 df.drop(0, axis=0).head()
 
 delete_indexes = [1, 2, 3, 4]
 df.drop(delete_indexes, axis=0).head(10)
+
+# df = df.drop(delete_indexes, axis=0) #kalıcı hale gelir
+# df.drop(delete_indexes, axis=0, inplace=True) #kalıcı hale gelir inplace yaygın kullanılır
+
+#degiskeni index yapmak
+
+df["age"].head()
+df.age.head()
+
+df.index = df["age"]
+
+df.drop("age", axis=1).head() #axis=0 satır axis=1 sütun
+
+df.drop("age", axis=1, inplace=True)
+df.head()
+
+#indexi değişken yapmak 
+
+df.index
+
+df["age"] = df.index
+
+df.head()
+df.drop("age", axis=1, inplace=True)
+
+df.reset_index().head()
+df = df.reset_index()
+df.head()
+
+#degisken üzerinde islemler
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+"age" in df
+
+df["age"].head()
+df.age.head()
+
+df["age"].head()
+type(df["age"].head())#pandas serisi
+
+
+df[["age"]].head()
+type(df[["age"]].head())#dataframe olarak kalır
+
+df[["age", "alive"]]
+
+col_names = ["age", "adult_male", "alive"]
+df[col_names]
+
+df["age2"] = df["age"]**2 #yenidegisken eklenmis olur
+df["age3"] = df["age"] / df["age2"]
+
+df.drop("age3", axis=1).head()
+
+df.drop(col_names, axis=1).head()
+
+df.loc[:, ~df.columns.str.contains("age")].head() # ~ bunun dışındaki hepsini seç
+
+
+#loc ve iloc
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+# iloc: integer based selection index bilgisi vererek seçim yapma
+df.iloc[0:3]
+df.iloc[0, 0]
+
+# loc: label based selection label vererek seçim yapma
+df.loc[0:3]
+
+df.iloc[0:3, 0:3]
+df.loc[0:3, "age"]
+
+col_names = ["age", "embarked", "alive"]
+df.loc[0:3, col_names]
+
+#kosullu secim
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df[df["age"] > 50].head()
+df[df["age"] > 50]["age"].count() #saydırma
+
+df.loc[df["age"] > 50, ["age", "class"]].head()
+
+df.loc[(df["age"] > 50) & (df["sex"] == "male"), ["age", "class"]].head()
+
+df["embark_town"].value_counts()
+
+df_new = df.loc[(df["age"] > 50) & (df["sex"] == "male")
+       & ((df["embark_town"] == "Cherbourg") | (df["embark_town"] == "Southampton")),
+       ["age", "class", "embark_town"]]
+
+df_new["embark_town"].value_counts()
+
+#toplulaştırma ve gruplama
+
+#fonksiyonlar group by işlemi ile kullanılır
+# - count()
+# - first()
+# - last()
+# - mean()
+# - median()
+# - min()
+# - max()
+# - std()
+# - var()
+# - sum()
+# - pivot table
+
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age"].mean()
+
+df.groupby("sex")["age"].mean() #önce gruplanır sonra toplulaştırılır
+
+df.groupby("sex").agg({"age": "mean"})
+df.groupby("sex").agg({"age": ["mean", "sum"]})
+
+df.groupby("sex").agg({"age": ["mean", "sum"],
+                       "survived": "mean"})
+
+
+df.groupby(["sex", "embark_town"]).agg({"age": ["mean"],"survived": "mean"})
+
+df.groupby(["sex", "embark_town", "class"]).agg({"age": ["mean"],"survived": "mean"})
+
+
+df.groupby(["sex", "embark_town", "class"]).agg({"age": ["mean"],"survived": "mean","sex": "count"})
+
+
+#pivot table
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df.pivot_table("survived", "sex", "embarked") #ön tanımlı değer ortalamadır
+
+df.pivot_table("survived", "sex", "embarked",aggfunc="std")
+
+df.pivot_table("survived", "sex", ["embarked", "class"])
+
+df.head()
+
+df["new_age"] = pd.cut(df["age"], [0, 10, 18, 25, 40, 90]) #sayısal değişkeni kategorik yapma değişkeni tanıyorsak cut tanımıyorsak qcut
+
+df.pivot_table("survived", "sex", ["new_age", "class"])
+
+pd.set_option('display.width', 500)
+
+
+#lambda &apply 
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age2"] = df["age"]*2
+df["age3"] = df["age"]*5
+
+(df["age"]/10).head()
+(df["age2"]/10).head()
+(df["age3"]/10).head()
+#fonksiyon  ile yapımı
+for col in df.columns:
+    if "age" in col:
+        print(col)
+
+for col in df.columns:
+    if "age" in col:
+        print((df[col]/10).head())
+
+for col in df.columns:
+    if "age" in col:
+        df[col] = df[col]/10
+
+df.head()
+#apply ile uygulama
+df[["age", "age2", "age3"]].apply(lambda x: x/10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: x/10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: (x - x.mean()) / x.std()).head()
+
+def standart_scaler(col_name):
+    return (col_name - col_name.mean()) / col_name.std()
+
+df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head() #fonksiyonlarda kullanılır
+
+# df.loc[:, ["age","age2","age3"]] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+
+df.loc[:, df.columns.str.contains("age")] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+
+df.head()
+
+#birleştirme işlemleri  
+
+import numpy as np
+import pandas as pd
+m = np.random.randint(1, 30, size=(5, 3))
+df1 = pd.DataFrame(m, columns=["var1", "var2", "var3"])
+df2 = df1 + 99
+
+pd.concat([df1, df2])
+
+pd.concat([df1, df2], ignore_index=True)
+
+#merge ile birleştirme
+
+df1 = pd.DataFrame({'employees': ['john', 'dennis', 'mark', 'maria'],
+                    'group': ['accounting', 'engineering', 'engineering', 'hr']})
+
+df2 = pd.DataFrame({'employees': ['mark', 'john', 'dennis', 'maria'],
+                    'start_date': [2010, 2009, 2014, 2019]})
+
+pd.merge(df1, df2)
+pd.merge(df1, df2, on="employees") #çalşanlara göre birleştir
+
+# her calısanın müdür bilgisi
+df3 = pd.merge(df1, df2)
+
+df4 = pd.DataFrame({'group': ['accounting', 'engineering', 'hr'],
+                    'manager': ['Caner', 'Mustafa', 'Berkcan']})
+
+pd.merge(df3, df4)
 
 
 
